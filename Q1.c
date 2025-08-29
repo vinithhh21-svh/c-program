dda500830fb37829dd86f95fbca98bd59cd28f30
vinithhh21-svh <vinithhh21@gmail.com>
2025-08-28 23:37:08 -0700

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{char str[20]
+scanf("%s",str);
+printf("%"s,str);
+}
---
+++ Q3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{char str[40];
+scanf("%[^\n]s",str);
+printf("%s",str);
+}
